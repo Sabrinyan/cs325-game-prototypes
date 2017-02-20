dff2fe45d1ac9095650951aab8a047bfd9741053
Sabrinyan <sabrinyan@gmail.com>
2017-02-20 18:50:57 -0500

    youre gonna have to make it look pretty tho dood

--- Assignment3/js/main.js
+++ Assignment3/js/main.js
@@ -25,6 +25,8 @@ window.onload = function() {
     var door1;
     var door2;
     var door3;
+    var doorX;
+    var doorY;
 
     var d1 = false;
     var d2 = false;
@@ -32,18 +34,25 @@ window.onload = function() {
 
     var cntLevel;
     var cntSec;
-    var door;
     var loopTime;
     var cont;
 
+    var worldScale = 1;
+
     var text;
     var textLevel;
+    var textTime;
     
+    var camX;
+    var camY;
+    var timer;
+    var event;
+
     function create() {
 
         cntLevel = 1;
         cont = true;
-        cntSec = 0;
+        cntSec = 10;
         // Create a sprite at the center of the screen using the 'logo' image.
         door1 = game.add.sprite(game.world.centerX - 250, game.world.centerY, 'door1');
         door2 = game.add.sprite(game.world.centerX, game.world.centerY, 'door2');
@@ -58,12 +67,21 @@ window.onload = function() {
         door3.inputEnabled = true;
         
         text = game.add.text(game.world.centerX, 15, "Click on a door and hope that luck is on your side!\nIf it's not you'll probably die!", { font: "25px Arial", fill: "#9999ff", align: "center" });
-        textLevel = game.add.text(game.world.centerX, 100, "Level: " + cntLevel, { font: "25px Arial", fill: "#9999ff", align: "center" });
+        textLevel = game.add.text(game.world.centerX, 115, "Level: " + cntLevel, { font: "25px Arial", fill: "#9999ff", align: "center" });
+        textTime = game.add.text(game.world.centerX, 85, "Time: " + cntSec, { font: "25px Arial", fill: "#9999ff", align: "center" });
         text.anchor.setTo(0.5, 0.0);
-        textLevel.anchor.setTo(0.5, 0.0)
+        textLevel.anchor.setTo(0.5, 0.0);
+        textTime.anchor.setTo(0.5, 0.0);
 
+        timer = game.time.create(true);
+        //timer = game.time.events.loop(Phaser.Timer.SECOND * .01, zoom, this);
+        //event = timer.loop(Phaser.Timer.SECOND * .01, zoom, this);
+        timer.start();
         game.time.events.loop(Phaser.Timer.SECOND, time, this);
         randomDoor();
+        
+        camX = game.world.width / 2;
+        camY = game.world.height - 400;
     }
     
     function update() {
@@ -72,53 +90,62 @@ window.onload = function() {
         door2.events.onInputDown.add(gone2);
         door3.events.onInputDown.add(gone3);
 
-        if (cntSec == 10 && cntLevel < 11) {
+        if (cntSec == 0 && cntLevel < 11) {
+            
+            timer.remove(event);
+            game.world.scale.set(1);
+
             if (d1 == false && d2 == false && d3 == false)
                 death();
             else {
+                //event = timer.loop(Phaser.Timer.SECOND * .01, zoom, this);
+                //timer.start();
                 randomDoor();
-                cntSec = 0;
+                d1 = false;
+                d2 = false;
+                d3 = false;
             }
         }
 
         if (cntLevel == 10) {
             win();
         }
-
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 
     function time() {
-        cntSec++;
+        
+        cntSec--;
+        textTime.setText("Time: " + cntSec);
+    }
+
+    function zoom() {
+        worldScale += 0.01;
+        worldScale = Phaser.Math.clamp(worldScale, 0.25, 1.5);
+        game.world.scale.set(worldScale);
+        game.camera.focusOnXY(camX * game.world.scale.x, camY * game.world.scale.y);
+
     }
 
     function randomDoor() {
 
-        door = Math.floor(Math.random() * 3) + 1;
+        doorX = Math.floor(Math.random() * 3) + 1;
+        doorY = Math.floor(Math.random() * 3) + 1;
 
-        if (door == 1) {
+        if (doorX == 1 || doorY == 1) {
             d1 = true;
-            d2 = false;
-            d3 = false;
         }
-        if (door == 2) {
-            d1 = false;
+        if (doorX == 2 || doorY == 2) {
             d2 = true;
-            d3 = false;
         }
-        if (door == 3) {
-            d1 = false;
-            d2 = false;
+        if (doorX == 3 || doorY == 3) {
             d3 = true;
         }
     }
 
     function gone1() {
+
+        cntSec = 10;
+        textTime.setText("Time: " + cntSec);
 
         if (d1 == true) {
             cntLevel++;
@@ -135,6 +162,9 @@ window.onload = function() {
 
     function gone2() {
 
+        cntSec = 10;
+        textTime.setText("Time: " + cntSec);
+
         if (d2 == true) {
             cntLevel++;
             textLevel.setText("Level: " + cntLevel);
@@ -149,6 +179,9 @@ window.onload = function() {
     }
 
     function gone3() {
+
+        cntSec = 10;
+        textTime.setText("Time: " + cntSec);
 
         if (d3 == true) {
             cntLevel++;
