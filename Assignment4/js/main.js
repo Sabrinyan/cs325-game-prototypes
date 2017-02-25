697ac9c6a348d1ce3b4dc64d1a512571a8e96429
Sabrinyan <sabrinyan@gmail.com>
2017-02-25 18:14:38 -0500

    sound?

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -32,6 +32,10 @@ window.onload = function() {
         game.load.image('stressbar', 'assets/healthbar.png');
         game.load.image('stressborder', 'assets/border.png');
         game.load.image('background', 'assets/background.png');
+
+        game.load.spritesheet('boi', 'assets/littleboy.png', 100, 240, 3);
+
+        game.load.audio('phonesound', 'assets/soundeffects/phone.ogg');
     }
     
     var aclock;
@@ -42,12 +46,16 @@ window.onload = function() {
     var speaker;
     var table;
     var tv;
+
+    var boy;
     
     var stressbar;
     var border;
     var stress = 0;
 
     var text;
+
+    var phonesound;
 
     function create() {
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
@@ -67,6 +75,10 @@ window.onload = function() {
         speaker = game.add.sprite(game.world.centerX - 150, game.world.centerY + 175, 'speaker');
         tv = game.add.sprite(game.world.centerX - 320, game.world.centerY + 90, 'tv');
 
+        boy = game.add.sprite(game.world.centerX-25, game.world.centerY, 'boi');
+        //boy.animations.add('sad');
+        //boy.animations.play('sad', 1, true);
+
         aclock.anchor.setTo(0.5, 0.5);
         gclock.anchor.setTo(0.5, 0.5);
         laptop.anchor.setTo(0.5, 0.5);
@@ -84,6 +96,13 @@ window.onload = function() {
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
 
+        aclock.inputEnabled = true;
+        gclock.inputEnabled = true;
+        laptop.inputEnabled = true;
+        phone.inputEnabled = true;
+        speaker.inputEnabled = true;
+        tv.inputEnabled = true;
+
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
@@ -91,6 +110,8 @@ window.onload = function() {
         text.anchor.setTo(0.5, 0.0);
 
         // When you click on the sprite, you go back to the MainMenu.
+        phonesound = game.add.audio('phonesound');
+        phonesound.play();
     }
     
     function update() {
